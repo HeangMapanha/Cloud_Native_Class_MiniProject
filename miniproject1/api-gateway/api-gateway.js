9b063e2968a6e2566a055dcd02a1101f58386a84
Heang Mapanha <Mapanha.heang@gmail.com>
2025-05-29 21:32:24 +0700

    ss

--- miniproject1/api-gateway/api-gateway.js
+++ miniproject1/api-gateway/api-gateway.js
@@ -6,13 +6,13 @@ const httpProxy = require('http-proxy')
 const proxy = httpProxy.createProxyServer();
 
 //REDIRECT TO THE STUDENT MICROSERVICE
-app.use('/student', (req, res) => {
+app.all('/student', (req, res) => {
     console.log("INSIDE API GATEWAY STUDENT ROUTE")
     proxy.web(req, res, { target: 'http://localhost:5000' });
 })
 
 //REDIRECT TO THE TEACHER MICROSERVICE
-app.use('/teacher', (req, res) => {
+app.all('/teacher', (req, res) => {
     console.log("INSIDE API GATEWAY TEACHER ROUTE")
     proxy.web(req, res, { target: 'http://localhost:5001' });
 })
