3bacbcdc71ae42700186e33d609d7610e3bf5d22
Heang Mapanha <Mapanha.heang@gmail.com>
2025-05-31 14:01:25 +0700

    ip change

--- miniproject1/api-gateway/api-gateway.js
+++ miniproject1/api-gateway/api-gateway.js
@@ -8,13 +8,13 @@ const proxy = httpProxy.createProxyServer();
 //REDIRECT TO THE STUDENT MICROSERVICE
 app.use('/student', (req, res) => {
     console.log("INSIDE API GATEWAY STUDENT ROUTE")
-    proxy.web(req, res, { target: 'http://localhost:5000' });
+    proxy.web(req, res, { target: 'http://3.87.245.121:5000' });
 })
 
 //REDIRECT TO THE TEACHER MICROSERVICE
 app.use('/teacher', (req, res) => {
     console.log("INSIDE API GATEWAY TEACHER ROUTE")
-    proxy.web(req, res, { target: 'http://localhost:5001' });
+    proxy.web(req, res, { target: 'http://34.204.18.186:5001' });
 })
 
 app.listen(4000,() => {
