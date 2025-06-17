9b21e51ac2b093214488023ce2f87b3ebb3cbfd8
Heang Mapanha <Mapanha.heang@gmail.com>
2025-06-17 11:30:03 +0700

    dsd

--- Customer/index.js
+++ Customer/index.js
@@ -29,7 +29,7 @@ app.get('/product/:product_name', async (req, res) => {
       return res.status(404).json({ message: 'Product not found' });
     }
 
-    res.status(200).json(product.product_name);
+    res.status(200).json(product);
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
